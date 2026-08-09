d4d9e0f56c18993e6e064e4ce75a6e551ee62397
mohamed <msyd32831@gmail.com>
2026-08-09 15:49:16 +0300

    feat: initialize counter component with increment and decrement functionality

--- counter-cli/vue-counter/src/App.vue
+++ counter-cli/vue-counter/src/App.vue
@@ -1,11 +1,16 @@
 <script setup>
-const counter = 0;
+import { ref } from "vue";
+const counter = ref(0);
 
+
+const decrement = () => {
+  counter.value--;
+};
 </script>
 
 <template>
   <h1>Counter is {{ counter }}</h1>
-  <button @click="increment">Increment +1</button>
+  <button @click="counter++">Increment +1</button>
   <button @click="decrement">Decrement -1</button>
 </template>
 
