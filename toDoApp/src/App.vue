a7c4d2555c4167569ed76a4a4b44c3a76c0ff824
mohamed <msyd32831@gmail.com>
2026-08-09 21:49:41 +0300

    feat: initialize Vue project with basic task creation functionality

--- toDoApp/src/App.vue
+++ toDoApp/src/App.vue
@@ -1,11 +1,57 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+const newTask = ref('');
 
+const tasks=ref([])
+// here push the data into the ref array
+
+function addTask(){
+  const text=newTask.value.trim();
+  if(!text)return;
+  tasks.value.push({
+    id:Date.now(),
+    text:text,
+    completed:false,
+    favorite:false,
+    });
+    newTask.value=''
+  }
+
+</script>
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+ <div id="wrapper">
+  <h1>Todo App</h1>
+  <div class="input-row">
+    <!-- v- model it is way to binding data known as two binding way-->
+    <input type="text" placeholder="add task here" v-model="newTask">
+    <button>Add</button>
+  </div>
+ </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.app{
+  max-width: 500px;
+  margin: 2rem auto;
+  font-family: sans-serif;
+  text-align: center;
+}
+.input-row{
+  display: flex;
+  gap:0.5rem;
+  margin-bottom: 1rem;
+}
+input{
+  flex-grow:1;
+  padding:0.5rem;
+  border-radius: 6px;
+  border: 1px solid #ccc;
+
+}
+button{
+  padding:0.5rem 1rem;
+  border-radius: 6px;
+  border: 1px solid #ccc;
+  cursor: pointer;
+}
+</style>
