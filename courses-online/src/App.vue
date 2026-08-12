765b81d3951c9bce38ef622702e9e24e1dbb1777
mohamed <msyd32831@gmail.com>
2026-08-12 19:05:31 +0300

    feat: initialize Vue 3 project with Vite, Tailwind CSS, and linting configuration

---
+++ courses-online/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+
+<div class="p-4 space-y-8">
+  <h1 class="text-4xl font-medium">cources hub</h1>
+<h2 class="text-2xl font-medium">all courcess</h2>
+<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+  <course-items v-for="i in 3"></course-items>
+
+</div> <h2 class="text-2xl font-medium">your cources</h2>
+</div>
+
+</template>
+<script setup>
+import CourseItems from "@/components/CourseItems.vue";
+</script>
+ 
---
+++ courses-online/src/components/CourseItems.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="bg-white border border-gray-200 rounded-md">
+        <h3 class="p-4 text-medium border-b border-gray-200">{{ title }}</h3>
+        <p class="p-4 text-medium border-b border-gray-200">{{ price }}</p>
+        <p class="p-4 text-medium">{{ description }}</p>
+    </div>
+</template>
+<script setup> defineProps({
+    title: String,           
+    price: Number,
+    description: String,
+}
+    
+)</script>
