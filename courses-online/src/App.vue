250a2e114216e14e2ce310a20b8e75b05a73ca2c
mohamed <msyd32831@gmail.com>
2026-08-14 04:48:08 +0300

    feat: implement main application layout with dynamic course fetching and booking item list

--- courses-online/src/App.vue
+++ courses-online/src/App.vue
@@ -1,28 +1,35 @@
 <template>
 
-<div class="p-4 space-y-8">
-  <h1 class="text-4xl font-medium">cources hub</h1>
-<h2 class="text-2xl font-medium">all courcess</h2>
-<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-  <course-items 
-  v-for="i in 3"
-  :key="i"
-  title="vue crash course"
-  price="100"
-  description="learn from scratch and master the fundamentals of vue"
-  @click="console.log('button clicked')"
-  ></course-items>
+  <div class="p-4 space-y-8">
+    <h1 class="text-4xl font-medium">cources hub</h1>
+    <h2 class="text-2xl font-medium">all courcess</h2>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <course-items v-for="course in courses" :key="course.id" :title="course.title" :price="course.price"
+        :description="course.description" @click="console.log('button clicked')"></course-items>
 
-</div> 
-<h2 class="text-2xl font-medium">your cources</h2>
-<div class="grid grid-cols-1 gap-4">
-<BookingItem v-for="i in 2" :key="i" />
-</div>
-</div>
+    </div>
+    <h2 class="text-2xl font-medium">your cources</h2>
+    <div class="grid grid-cols-1 gap-4">
+      <BookingItem v-for="i in 2" :key="i" />
+    </div>
+  </div>
 
 </template>
 <script setup>
+import { ref, onMounted } from "vue";
 import CourseItems from "@/components/CourseItems.vue";
 import BookingItem from "@/components/boockingItem.vue";
+
+
+const courses = ref([])
+const fetchCources = async () => {
+
+  const res = await fetch("http://localhost:5000/courses")
+  courses.value = await res.json()
+
+}
+
+onMounted(() => {
+  fetchCources()
+})
 </script>
- 
