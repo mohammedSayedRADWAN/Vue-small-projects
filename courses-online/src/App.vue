4be3948a8d058ef18de2d4e16ef37f0ed66694bb
mohamed <msyd32831@gmail.com>
2026-08-14 05:11:44 +0300

    feat: implement skeleton loader for course data fetching in App component

--- courses-online/src/App.vue
+++ courses-online/src/App.vue
@@ -3,6 +3,10 @@
   <div class="p-4 space-y-8">
     <h1 class="text-4xl font-medium">cources hub</h1>
     <h2 class="text-2xl font-medium">all courcess</h2>
+    <div v-if="flag" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <SkelatonCourses v-for="i in 4"></SkelatonCourses>
+
+    </div>
     <div v-if="flag" class="text-2xl font-medium">courcess are loading...</div>
     <div v-else class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
       <course-items v-for="course in courses" :key="course.id" :title="course.title" :price="course.price"
@@ -20,6 +24,7 @@
 import { ref, onMounted } from "vue";
 import CourseItems from "@/components/CourseItems.vue";
 import BookingItem from "@/components/boockingItem.vue";
+import SkelatonCourses from "@/components/SkelatonCourses.vue";
 
 
 const courses = ref([])
